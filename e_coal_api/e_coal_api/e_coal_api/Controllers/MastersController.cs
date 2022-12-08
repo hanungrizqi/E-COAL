35595cea7c99fab8a8b955a7251ec5a536c73228
Fajar Priambodo <fajar.priambodo@kppmining.com>
2022-12-08 16:14:37 +0700

    penmabahan dbml

---
+++ e_coal_api/e_coal_api/e_coal_api/Controllers/MastersController.cs
@@ -0,0 +1,30 @@
+﻿using e_coal_api.View_Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace e_coal_api.Controllers
+{
+    public class MastersController : ApiController
+    {
+        [HttpGet]
+        [Route("api/Masters/getListProfile")]
+        public IHttpActionResult getListProfile()
+        {
+            try
+            {
+                ClsProfile clsProfile = new ClsProfile();
+                var data = clsProfile.getProfile();
+
+                return Ok(new { Status = true, Data = data });
+            }
+            catch(Exception e)
+            {
+                return Ok(new { Status = false, Error = e.ToString() });
+            }
+        }
+    }
+}
---
+++ e_coal_api/e_coal_api/e_coal_api/View_Model/ClsProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc.Ajax;
+using e_coal_api.Models;
+
+namespace e_coal_api.View_Model
+{
+	public class ClsProfile
+	{
+        db_eCoalDataContext db = new db_eCoalDataContext();
+
+        public string PROFILE { get; set; }
+
+        public IQueryable<TBL_M_PROFILE> getProfile()
+        {
+            var data = db.TBL_M_PROFILEs.ToList().AsQueryable();
+            return data;
+        }
+    }
+}
